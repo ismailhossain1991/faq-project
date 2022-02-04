c5ce7b71e7cf3d9c316e521a5cfa121689db5bc2
ismailhossain1991 <ismail017456@gmail.com>
2022-02-04 20:39:23 +0600

    all file upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const acc_header = document.querySelectorAll('.accordion-header');
+
+
+acc_header.forEach(item =>{
+    item.addEventListener('click', function(){
+        item.classList.toggle('active');
+
+        if(item.classList.contains('active')){
+        item.nextElementSibling.style.height =  item.nextElementSibling.scrollHeight + 'px';
+        }else{
+            item.nextElementSibling.style.height = '0px';
+        }
+    });
+});
