075fc1c59af26767965af69f3561e857eb65050b
ismailhossain1991 <ismail017456@gmail.com>
2022-02-05 20:42:32 +0600

    new file all upload

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const acc_header = document.querySelectorAll('.accordion-header');
 
 acc_header.forEach(item =>{
     item.addEventListener('click', function(){
-        item.classList.toggle('active');
+        acc_header.forEach(item =>{
+        
+            if(item !=this){
+            item.classList.remove('active');
+            item.nextElementSibling.style.height = '0px'; 
+            }
+        
+        });
+       
+          item.classList.toggle('active');
 
         if(item.classList.contains('active')){
         item.nextElementSibling.style.height =  item.nextElementSibling.scrollHeight + 'px';
